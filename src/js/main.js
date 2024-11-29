f7c24a7d60de1d34073524ce7fc5947a5a78d425
Otabek Ortikov <147712639+OtabekOrtikov@users.noreply.github.com>
2024-11-29 14:52:45 +0500

    fixed bugs, fontello icon library, about section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,28 @@
 import { showSlides } from "./slide.js";
 
 showSlides();
+
+const submenu = document.querySelector(".submenu");
+const submenuList = document.querySelector(".submenu-list");
+const submenuBtn = document.querySelector(".submenu-btn");
+
+submenu.addEventListener("mouseenter", () => {
+  submenuList.classList.add("show");
+  submenuBtn.classList.add("active-submenu");
+});
+submenu.addEventListener("mouseleave", () => {
+  submenuList.classList.remove("show");
+  submenuBtn.classList.remove("active-submenu");
+});
+submenuList.addEventListener("click", () => {
+  submenuList.classList.remove("show");
+  submenuBtn.classList.remove("active-submenu");
+});
+
+const menu = document.querySelector(".header-nav");
+const hamburger = document.querySelector(".header-hamburger");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("show");
+  hamburger.classList.toggle("close-burger");
+});
